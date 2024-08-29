48787bc9b992590988ee2a4aaf42df8f8088bb59
zaelggk <zhdlcc@gmail.com>
2024-08-29 23:32:56 +0800

    ignore connect reset exception

--- src/pylogserver_aquanjsw/main.py
+++ src/pylogserver_aquanjsw/main.py
@@ -7,20 +7,23 @@ import struct
 
 class LogRecordStreamRequestHandler(socketserver.StreamRequestHandler):
     def handle(self):
-        while True:
-            # The meaning of the first 4 bytes:
-            # https://docs.python.org/zh-cn/3/library/logging.handlers.html#logging.handlers.SocketHandler.makePickle
-            chunk = self.request.recv(4)
-            # Invalid record, skip this log
-            if len(chunk) < 4:
-                break
-            data_len = struct.unpack('>L', chunk)[0]
-            chunk = self.request.recv(data_len)
-            while len(chunk) < data_len:
-                chunk = chunk + self.request.recv(data_len - len(chunk))
-            obj = self.unpickle(chunk)
-            record = logging.makeLogRecord(obj)
-            self.handle_log_record(record)
+        try:
+            while True:
+                # The meaning of the first 4 bytes:
+                # https://docs.python.org/zh-cn/3/library/logging.handlers.html#logging.handlers.SocketHandler.makePickle
+                chunk = self.request.recv(4)
+                # Invalid record, skip this log
+                if len(chunk) < 4:
+                    break
+                data_len = struct.unpack('>L', chunk)[0]
+                chunk = self.request.recv(data_len)
+                while len(chunk) < data_len:
+                    chunk = chunk + self.request.recv(data_len - len(chunk))
+                obj = self.unpickle(chunk)
+                record = logging.makeLogRecord(obj)
+                self.handle_log_record(record)
+        except ConnectionResetError:
+            pass
 
     @staticmethod
     def unpickle(data):
